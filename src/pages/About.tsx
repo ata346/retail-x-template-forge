57aa5e1a0661f3658a7b5134d99a399b4650ccf6
ata346 <adwebcomicagency@gmail.com>
2025-04-17 10:07:30 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -148,10 +148,10 @@ const About = () => {
                   With a passion for technology and business, Muhammed Adnan founded Retail X with a vision to democratize e-commerce for entrepreneurs worldwide. His innovative approach has transformed how businesses establish their online presence.
                 </p>
                 <div className="flex justify-center mt-6 space-x-4">
-                  <a href="https://linkedin.com" className="text-retail-600 hover:text-retail-800 transition-colors">
+                  <a href="https://www.linkedin.com/in/muhammedadnanvv/?locale=en_US" className="text-retail-600 hover:text-retail-800 transition-colors">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
                   </a>
-                  <a href="https://twitter.com" className="text-retail-600 hover:text-retail-800 transition-colors">
+                  <a href="https://x.com/MuhammadAd93421" className="text-retail-600 hover:text-retail-800 transition-colors">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-twitter"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"/></svg>
                   </a>
                 </div>
